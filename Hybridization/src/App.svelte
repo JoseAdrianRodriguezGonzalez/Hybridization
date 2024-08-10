df2ce58c89430dabedeea19a28cc65c7b7cf9a56
Jose Adrian <kerpador@gmail.com>
2024-08-10 14:46:40 -0600

    adding all files

--- Hybridization/src/App.svelte
+++ Hybridization/src/App.svelte
@@ -2,6 +2,7 @@
   
   import Header from './components/Header.svelte';
   import Main from './components/main.svelte';
+  import Footer from './components/footer.svelte'
 </script>
 
 <main>
@@ -12,4 +13,7 @@
     
   </Main>
 
+  <Footer>
+    
+  </Footer>
 </main>
---
+++ Hybridization/src/components/footer.svelte
@@ -0,0 +1,11 @@
+<script>
+    import './footer.css'
+</script>
+<footer>
+    <div class="">Contact us on</div>
+    <div class="">
+        <p>
+            The information about this site you can look it at at different source and check the fidelity of the terms
+        </p>
+    </div>
+</footer>
