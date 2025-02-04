9af6b33345aaa95fb2ea566ad870dc8816d7ced8
Jose Adrian <kerpador@gmail.com>
2025-02-04 01:58:02 -0600

    add some dependencies

--- Hybridization/src/routes/Periodic.svelte
+++ Hybridization/src/routes/Periodic.svelte
@@ -3,15 +3,35 @@
     import Header from '../components/Header.svelte';
     import Footer from '../components/footer.svelte';
     import  './styles.css';
-    const items =Array.from({length:108},(_,i)=>i+1);
+  import { onMount } from 'svelte';
+    const items =Array.from({length:126},(_,i)=>i+1);
     const items2=Array.from({length:28},(_,i)=>i+1);
-    
+    let visibleItems=[];
+    onMount(()=>{
+        let counter=1;
+        visibleItems =Array.from(document.querySelectorAll(".box")).map((el)=>{
+            if(window.getComputedStyle(el).visibility!=='hidden'){
+                if (counter==57 ||counter==89){
+                    let temp=counter+14
+                     counter=temp+1;
+                     return temp;
+                }else{
+                    return counter++; 
+                }
+            } 
+            
+            return null;
+        })
+        
+
+        console.log("Numeración continua de los visibles:", visibleItems);
+    })
 </script>
 <Header></Header>
 <div class="Periodic">
-    {#each items as item}
+    {#each items as item,index}
         
-        <div class="box box-{item}">Div {item}</div>
+        <div class="box box-{index}">Div {visibleItems[index]!==null? visibleItems[index]:"-"}</div>
     {/each}
 </div>
 <div class="Periodic-2">
