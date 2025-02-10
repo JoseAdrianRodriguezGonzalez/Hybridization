7d409107f18898b8eaaf4e45513a624ee736b335
Jose Adrian <kerpador@gmail.com>
2025-02-09 22:48:17 -0600

    New modifications, Periodic table with elements, just lack the quantum numbers

---
+++ Hybridization/src/components/elements.svelte
@@ -0,0 +1,17 @@
+<script>
+    import './elements.css';
+    export let atomicNumber=0;
+    export let elementName="Hydrogen";
+    export let Symbol="H";
+</script>
+<div class="main">
+    <div class="top">
+        <div>{atomicNumber}</div>
+        <div>{elementName}</div>
+    </div>
+    <div class="mid">
+        <div></div>
+        <div></div>
+    </div>
+    <div class="down">{Symbol}</div>
+</div>
